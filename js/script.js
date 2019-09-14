dfdb236ee02cd8884e347c4033546c38cb6d16db
DMergen <50367534+DMergen@users.noreply.github.com>
2019-09-15 00:18:50 +0600

    Adaptive

--- js/script.js
+++ js/script.js
@@ -3,13 +3,35 @@ $(function() {
   var owl = $(".owl-carousel");
   owl.owlCarousel({
     items: 1, 
-    autoWidth: true,
+    margin: 10,
+    loop: true,
+    autoplay:true, 
+    autoplayHoverPause:true, 
+    smartSpeed:1000,
     nav: true,
     dots: false,
     navText: [
 	  '<img src="img/left-nav.png">',
 	  '<img src="img/right-nav.png">'
-	 ]
+	  ],
+    responsive: {
+      0: {
+        nav: false,
+        dots: true
+      },
+      480: {
+        nav: true,
+        dots: false
+      }
+    }
   });
 
 });
+
+var wrapperMenu = document.querySelector('.hamburger');
+var menuSlide = document.querySelector('.menu-list')
+
+wrapperMenu.addEventListener('click', function(){
+  wrapperMenu.classList.toggle('is-active');
+  menuSlide.classList.toggle('menuS') ;
+})
